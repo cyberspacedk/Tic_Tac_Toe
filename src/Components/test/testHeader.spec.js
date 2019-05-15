aeffafdc40ad23f66cb2b6be139a726a22f4f62b
cyberspacedk <frontend.dk@gmail.com>
2019-05-15 14:28:41 +0300

    add editorconfig and write test for header

--- src/Components/test/testHeader.spec.js
+++ src/Components/test/testHeader.spec.js
@@ -2,9 +2,11 @@ import React from "react";
 import { shallow } from "enzyme";
 import Header from "../Header";
 
-describe("Header component", ()=> {
-    it("Content should be - TIC TAC TOE", ()=>{
-        const headerComponent = shallow(<Header />); 
-        expect(headerComponent.text()).toEqual('TIC TAC TOE');
-    })
-})
+describe("Header component", () => {
+  // 1- check type of component content
+
+  it("Content should be - TIC TAC TOE", () => {
+    const headerComponent = shallow(<Header />);
+    expect(headerComponent.text()).toEqual("TIC TAC TOE");
+  });
+});
